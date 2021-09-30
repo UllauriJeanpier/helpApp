e080e6cce4aa4a5347715a1bc3ad2516fc5570ab
jean <ullauri_jeanalex@gmail.com>
2021-09-29 19:28:15 -0500

    [refactored] validateFunctions

--- App.tsx
+++ App.tsx
@@ -5,13 +5,16 @@ import { NavigationContainer } from '@react-navigation/native'
 import StackNavigator from './src/navigation/StackNavigator'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { COLORS } from './src/utils/constants'
+import DrawerNavigator from './src/navigation/DrawerNavigator'
 
 export default function App () {
   return (
+
     <SafeAreaProvider>
       <NavigationContainer>
         <StatusBar backgroundColor={ COLORS.PRIMARY }/>
         <StackNavigator />
+        { /* <DrawerNavigator /> */ }
       </NavigationContainer>
     </SafeAreaProvider>
 
