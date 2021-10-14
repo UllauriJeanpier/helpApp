b66005cdebed1aa3d8e9f003b31c59f253e29bc1
jean <ullauri_jeanalex@gmail.com>
2021-10-14 11:28:00 -0500

    [adding] NumberScreen and ProfileScreen Styling

--- src/screens/Home/NumberScreen.tsx
+++ src/screens/Home/NumberScreen.tsx
@@ -1,14 +1,80 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity, Linking } from 'react-native'
+import Header from '../../components/Header'
 
 const NumberScreen = () => {
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <SafeAreaView style={ styles.container }>
+      <Header title="Número de ayuda"/>
+      <View style={ styles.body }>
+        <Text style={ styles.description }>
+          A traves de estos números de teléfonos usted
+          podrá solicitar ayuda y realizar denuncias
+        </Text>
+        <View style={ styles.number }>
+          <Text style={ styles.text }>Teléfono:</Text>
+          <TouchableOpacity
+            style={ styles.button }
+            onPress={ async () => await Linking.openURL('tel:+067451520') }
+          >
+            <Text style={ styles.buttonText }>067 451520</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={ styles.number }>
+          <Text style={ styles.text }>Whatsapp:</Text>
+          <TouchableOpacity
+            style={ styles.button }
+            onPress={ async () => await Linking.openURL('https://wa.link/bqgp1z') }
+          >
+            <Text style={ styles.buttonText }>980 121 194</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   )
 }
 
 export default NumberScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFCF7'
+  },
+  body: {
+    paddingTop: 25,
+    paddingHorizontal: 28
+  },
+  description: {
+    color: '#000',
+    fontSize: 15,
+    fontWeight: 'bold',
+    marginBottom: 26
+  },
+  number: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottomColor: '#216D3F',
+    borderBottomWidth: 1,
+    paddingVertical: 30
+  },
+  text: {
+    color: '#3A413D',
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#216D3F',
+    borderRadius: 20,
+    height: 40,
+    justifyContent: 'center',
+    width: 170
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold'
+  }
+})
--- src/screens/Home/ProfileScreen.tsx
+++ src/screens/Home/ProfileScreen.tsx
@@ -1,14 +1,116 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, ScrollView, Text, View, SafeAreaView, TouchableOpacity } from 'react-native'
 
-const ProfileScreen = () => {
+import Header from '../../components/Header'
+// UserPhoto es local, usar cualquier otra foto.
+import UserPhoto from '../../assets/svg/User-yanapakun.svg'
+import Camera from '../../assets/svg/Camara.svg'
+import { SCREEN } from '../../utils/constants'
+
+interface Props {
+  name: string
+  age: number | string
+  document: number
+  district: string
+  email: string
+  phone: number
+  phoneEmergency: number
+  img: any
+}
+
+const ProfileScreen = ({ name, age, document, district, email, img, phone, phoneEmergency }: Props) => {
   return (
-    <View>
-      <Text>ProfileScreen</Text>
-    </View>
+    <SafeAreaView style={ styles.container }>
+      <ScrollView>
+        <View>
+          <Header title="Perfil"/>
+          <View>
+            <View style={ styles.UserPhoto }>
+              { /* <Image
+                source={ img }
+              /> */ }
+              <UserPhoto />
+              <TouchableOpacity
+                style={ styles.saveUserPhoto }
+                onPress={ () => alert('click') }
+              >
+                <Camera />
+              </TouchableOpacity>
+            </View>
+            <View style={ styles.data }>
+              <View style={ styles.dataUser }>
+                <Text style={ styles.fontText }>Nombre:</Text>
+                <Text style={ styles.fontText }>Marisol Ochoa Antaurco</Text>
+              </View>
+              <View style={ styles.dataUser }>
+                <Text style={ styles.fontText }>Edad:</Text>
+                <Text style={ styles.fontText }>42 años</Text>
+              </View>
+              <View style={ styles.dataUser }>
+                <Text style={ styles.fontText }>DNI:</Text>
+                <Text style={ styles.fontText }>70659234</Text>
+              </View>
+              <View style={ styles.dataUser }>
+                <Text style={ styles.fontText }>Distrito:</Text>
+                <Text style={ styles.fontText }>San pedro</Text>
+              </View>
+              <View style={ styles.dataUser }>
+                <Text style={ styles.fontText }>E-mail:</Text>
+                <Text style={ styles.fontText }>marisolochoa@gmail.com</Text>
+              </View>
+              <View style={ styles.dataUser }>
+                <Text style={ styles.fontText }>Número de teléfono:</Text>
+                <Text style={ styles.fontText }>946845604</Text>
+              </View>
+              <View style={ styles.lastDataUser }>
+                <Text style={ styles.fontText }>Teléfono de emergencia:</Text>
+                <Text style={ styles.fontText }>946845604</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
 export default ProfileScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFCF7'
+  },
+  UserPhoto: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 44
+  },
+  saveUserPhoto: {
+    position: 'absolute',
+    alignSelf: 'flex-end',
+    left: (SCREEN.width * 2) / 3,
+    top: 160
+  },
+  data: {
+    paddingHorizontal: 28
+  },
+  dataUser: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderBottomColor: '#216D3F',
+    borderBottomWidth: 1,
+    paddingVertical: 18
+  },
+  lastDataUser: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 18
+  },
+  fontText: {
+    color: '#3A413D',
+    fontSize: 16,
+    fontWeight: 'bold'
+  }
+})
