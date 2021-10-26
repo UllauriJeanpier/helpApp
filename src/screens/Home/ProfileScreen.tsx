a9090116f32ce8998cea5804c96164ccbb5075d9
Carlito <rody.castillo.tacuri@gmail.com>
2021-10-26 16:29:29 -0500

    Advance with upload and picker img

--- src/screens/Home/ProfileScreen.tsx
+++ src/screens/Home/ProfileScreen.tsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react'
-import { StyleSheet, ScrollView, Text, View, SafeAreaView, TouchableOpacity } from 'react-native'
+import { StyleSheet, ScrollView, Text, View, SafeAreaView, TouchableOpacity, Image, Platform } from 'react-native'
 
 import Header from '../../components/Header'
-// UserPhoto es local, usar cualquier otra foto.
 import UserPhoto from '../../assets/svg/User-yanapakun.svg'
 import Camera from '../../assets/svg/Camara.svg'
 import { SCREEN } from '../../utils/constants'
-import { getProfile } from '../../services/yanapakun/profile'
+import { getProfile, getProfilePhoto, uploadImage } from '../../services/yanapakun/profile'
 import Loading from '../../components/Loading'
+
+import * as ImagePicker from 'expo-image-picker';
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { IUserLogin } from '../../interfaces/authInterfaces'
 
 interface UserData {
   email: string
@@ -59,11 +62,54 @@ const ProfileScreen = ({ navigation }: any) => {
       console.log(e)
     }
   }
+  const [ image, setImage ] = useState<string>('')
+
+  const fetchPhotoUser = async () => {
+    try {
+      setLoading(true)
+      const user = await AsyncStorage.getItem('user')
+      let dataUser: IUserLogin
+      if (typeof user === 'string') {
+        dataUser = JSON.parse(user)
+        const response = await getProfilePhoto(dataUser?.id)
+        console.log(response);
+        
+      }
+    } catch (e) {
+      console.log(e)
+    }
+  }
   useEffect(() => {
     fetchData().then(() => {
       setLoading(false)
     })
   }, [])
+  useEffect(() => {
+    fetchPhotoUser()
+    pickImg()
+  }, [])
+  const pickImg = async () => {
+      if(Platform.OS==='ios') {
+          const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+          if ( status !== 'granted' ) {
+            alert('Permiso denegado - Yanapakun')
+          }
+      }
+  }
+
+  const pickerPicture = async () => {
+      const resp = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+          allowsEditing: true,
+          aspect: [1, 2],
+          quality: 0.8,
+      });
+      if(resp.cancelled) return;
+      if(!resp.uri) return;
+      console.log(resp);
+      setImage(resp.uri)
+      uploadImage(resp.uri, profile.id)
+  }
   return (
     <Loading loading={ loading }>
       <SafeAreaView style={ styles.container }>
@@ -71,17 +117,30 @@ const ProfileScreen = ({ navigation }: any) => {
           <View>
             <Header title="Perfil" icon={ 'menu' } action={ () => navigation.toggleDrawer() }/>
             <View>
-              <View style={ styles.UserPhoto }>
-                { /* <Image
-                source={ img }
-              /> */ }
-                <UserPhoto />
-                <TouchableOpacity
-                  style={ styles.saveUserPhoto }
-                  onPress={ () => alert('click') }
-              >
-                  <Camera />
-                </TouchableOpacity>
+              <View style={ styles.containPhotoUser }>
+                <View style={styles.UserPhoto}>
+                    {
+                      ( image )
+                      ?
+                      (
+                        <Image
+                            source={{ uri: 'https://yanapakunpolicia.com/users/profile/photo/2' }}
+                            style={ styles.usePickerPhoto }
+                        />
+                      ) 
+                      :
+                      (
+                        <UserPhoto />
+                      )
+                      
+                    }
+                    <TouchableOpacity
+                      style={ styles.saveUserPhoto }
+                      onPress={ pickerPicture }
+                    >
+                      <Camera />
+                    </TouchableOpacity>
+                </View>
               </View>
               <View style={ styles.data }>
                 <View style={ styles.dataUser }>
@@ -129,17 +188,32 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#FFFCF7'
   },
+  containPhotoUser: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   UserPhoto: {
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    marginVertical: 44
+    marginVertical: 44,
+    height: 200,
+    width: 180,
+    borderRadius: 20,
+    backgroundColor: 'transparent',
+    borderColor: '#e3e3e3',
+    borderWidth: 1,
   },
   saveUserPhoto: {
     position: 'absolute',
     alignSelf: 'flex-end',
-    left: (SCREEN.width * 2) / 3,
-    top: 160
+    right: -10,
+    bottom: -10
+  },
+  usePickerPhoto:{
+      width: 180,
+      height: 200,
+      borderRadius: 20
   },
   data: {
     paddingHorizontal: 28
--- src/services/yanapakun/profile.ts
+++ src/services/yanapakun/profile.ts
@@ -3,3 +3,27 @@ import Api from './api'
 export const getProfile = async () => {
   return await Api.get('/users/profile/data')
 }
+
+export const getProfilePhoto = async ( id: number ) => {
+  return await Api.get(`/users/profile/photo/${id}`)
+}
+
+// export const uploadImage = async (photo: string, id: number,)=> {
+
+//   // const formData = new FormData()
+//   // formData.append('photo',photo)
+
+//   // const body = {
+//   //   photo,
+//   //   "Content-Type": "multipart/form-data"
+//   // }
+   
+//   // try {
+//   //   const resp = await Api.post(`/users/profile/photo/${id}`, body)
+//   //   console.log({resp});
+    
+//   // } catch (error) {
+//   //   console.log({error});
+    
+//   // }
+// }
